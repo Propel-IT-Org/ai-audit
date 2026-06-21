41cebe19c42630ca6fd8d1308b53a824bc5a6ff5
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2026-06-21 23:35:34 +0600

    fix: prerendering error

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function AdminLayout({
@@ -6,7 +7,7 @@ export default async function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await auth.api.getSession();
+  const session = await auth.api.getSession({ headers: await headers() });
   if (!session || session.user.role !== "admin") redirect("/");
 
   return children;
